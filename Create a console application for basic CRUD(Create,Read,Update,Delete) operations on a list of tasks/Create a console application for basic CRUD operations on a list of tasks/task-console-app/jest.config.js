c59f237b09ed36a6eeef6841281092fb8177f8e3
mohamedjabrijs2005 <sit24ci006@sairamtap.edu.in>
2025-07-08 22:40:40 +0530

    Delete Create a console application for basic CRUD(Create,Read,Update,Delete) operations on a list of tasks/Create a console application for basic CRUD operations on a list of tasks/task-console-app directory

--- Create a console application for basic CRUD(Create,Read,Update,Delete) operations on a list of tasks/Create a console application for basic CRUD operations on a list of tasks/task-console-app/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  testMatch: ['**/src/**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-};
